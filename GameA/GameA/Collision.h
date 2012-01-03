c53071dbb849917e34bddced806bbcf3c90fc71f
acumen.vn@gmail.com <acumen.vn@gmail.com@cb78e1ee-b093-ba55-c4be-1a400e3dfaaf>
2012-01-03 17:56:18 +0000

    Thêm phần RECT trong Collision

--- GameA/GameA/Collision.h
+++ GameA/GameA/Collision.h
@@ -6,6 +6,7 @@ class CCollision
 public:
 	CCollision(void);
 	~CCollision(void);
+	RECT _Rectangle(float fX, float fY, int width, int height);
 	bool CheckCollisionBetween2Rect(RECT a, RECT b);
 	bool CheckStayInAnotherRect(RECT a, RECT b);
 };
