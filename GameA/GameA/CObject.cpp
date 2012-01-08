a8bc0bd005fa1e6ccc019432a5b754c331d8f41a
btn09520185 <btn09520185@cb78e1ee-b093-ba55-c4be-1a400e3dfaaf>
2012-01-08 16:09:18 +0000

    thay doi di chuyen monster

--- GameA/GameA/CObject.cpp
+++ GameA/GameA/CObject.cpp
@@ -113,8 +113,8 @@ CCObject::CCObject(float fX, float fY, char style)
 		m_checkedItem = true;
 		m_canBeDestroyed = true;
 		m_rect = _Rectangle(fX, fY, 40, 40);
-		m_workingArea = _Rectangle(fX-SizeTile*10, fY-SizeTile*10, SizeTile*20, SizeTile*20);
-		m_vX = 2;
+		m_workingArea = _Rectangle(fX-SizeTile*6, fY-SizeTile*6, SizeTile*12, SizeTile*12);
+		m_vX = 0;
 		m_vY = 0;
 		break;
 	case UNIT_MONSTER3:
@@ -122,7 +122,7 @@ CCObject::CCObject(float fX, float fY, char style)
 		m_checkedItem = true;
 		m_canBeDestroyed = true;
 		m_rect = _Rectangle(fX, fY, 40, 40);
-		m_workingArea = _Rectangle(fX-SizeTile*10, fY-SizeTile*10, SizeTile*20, SizeTile*20);
+		m_workingArea = _Rectangle(fX-SizeTile*6, fY-SizeTile*6, SizeTile*12, SizeTile*12);
 		m_vX = 2;
 		m_vY = 0;
 		break;
