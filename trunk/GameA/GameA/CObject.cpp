2b69e8dc4168c5134f912b9f481db1c587e52dd2
btn09520185 <btn09520185@cb78e1ee-b093-ba55-c4be-1a400e3dfaaf>
2012-01-09 17:20:54 +0000

    fix loi map

--- trunk/GameA/GameA/CObject.cpp
+++ trunk/GameA/GameA/CObject.cpp
@@ -96,7 +96,7 @@ CCObject::CCObject(float fX, float fY, char style)
 		m_checkedItem = true;
 		m_canBeDestroyed = true;
 		m_rect = _Rectangle(fX, fY, 40, 40);
-		m_workingArea = _Rectangle(fX-SizeTile*13, fY-SizeTile*10, SizeTile*20, SizeTile*20);
+		m_workingArea = _Rectangle(fX-SizeTile*10, fY-SizeTile*10, SizeTile*20, SizeTile*20);
 		m_vX = 2;
 		m_vY = 0;
 		break;
@@ -105,7 +105,7 @@ CCObject::CCObject(float fX, float fY, char style)
 		m_checkedItem = true;
 		m_canBeDestroyed = true;
 		m_rect = _Rectangle(fX, fY, 40, 40);
-		m_workingArea = _Rectangle(fX-SizeTile*6, fY-SizeTile*6, SizeTile*12, SizeTile*12);
+		m_workingArea = _Rectangle(fX-SizeTile*4, fY-SizeTile*4, SizeTile*8, SizeTile*8);
 		m_vX = 2;
 		m_vY = 0;
 		break;
